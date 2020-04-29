ce04d21fc1d2dada2cffd0dea17eaafbd810c1e7
YoNoSoySantiago <santiago.hurtado1@hotmail.com>
2020-04-29 08:50:26 -0500

    2

--- src/model/AlgorithmsRace.java
+++ src/model/AlgorithmsRace.java
@@ -16,24 +16,7 @@ public class AlgorithmsRace {
 	public void addArrayList(long n) {
 		arrayList.add(n);
 	}
-	public void addArrayListR(long n) {
-		addArrayListR(n,0);
-	}
-	private void addArrayListR(long n, int i) {
-		/*try {
-			arrayList.get(i);
-			addArrayListR(n,(i+1));
-		}catch(NullPointerException e) {
-			arrayList.set(i, n);
-		}
-		*/
-		if(arrayList.get(i)==null) {
-			arrayList.add(i,n);
-		}else {
-			addArrayListR( n,(i+1));
-		}
-				
-	}
+	
 	public void addLinkedList(long n) {
 		LinkedList nuevo = new LinkedList(n);
 		if(linkedList==null) {
@@ -72,11 +55,254 @@ public class AlgorithmsRace {
 			binaryTree = nuevo;
 		}else {
 			SearchBinaryTree actual = binaryTree;
-			if(n<actual.getNum()) {
+			
+			while(true) {
+				if(n<actual.getNum()) {
+					if(actual.getLeft()!=null) {
+						actual = actual.getLeft();
+					}else {
+						actual.setLeft(nuevo);
+						break;
+					}
+				}else {
+					if(actual.getRight()!=null) {
+						actual = actual.getRight();
+					}else {
+						actual.setRight(nuevo);
+						break;
+					}
+				}
+			}
+		}
+	}
+	
+	public void addBinaryTreeR(long n) {
+		if(binaryTree==null) {
+			binaryTree = new SearchBinaryTree(n);
+		}else {
+			addBinaryTreeR(n,binaryTree);
+		}
+		
+	}
+	private void addBinaryTreeR(long n,SearchBinaryTree actual) {
+		SearchBinaryTree nuevo = new SearchBinaryTree(n);
+		if(n<actual.getNum()) {
+			if(actual.getLeft()!=null) {
+				addBinaryTreeR(n,actual.getLeft());
+			}else {
+				actual.setLeft(nuevo);
+			}
+		}else {
+			if(actual.getRight()!=null) {
+				addBinaryTreeR(n,actual.getRight());
+			}else {
+				actual.setRight(nuevo);
+			}
+		}
+	}
+	
+	public boolean searchArrayList(long n) {
+		boolean result = false;
+		for (int i = 0; i < arrayList.size(); i++) {
+			if(arrayList.get(i)==n) {
+				result = true;
+				break;
+			}
+		}
+		return result;
+	}
+	
+	public boolean searchArrayListR(long n) {
+		if(arrayList.size()==0) {
+			return false;
+		}else {
+			return searchArrayListR(n,arrayList.size()-1);
+		}
+	}
+	
+	private boolean searchArrayListR(long n, int i) {
+		if(arrayList.get(i)==n) {
+			return true;
+		}else {
+			if(i>0) {
+				return searchArrayListR(n,i-1);
+			}
+			return false;
+		}
+	}
+	
+	public boolean searchLinkedList(long n) {
+		LinkedList actual = linkedList;
+		boolean result = false;
+		while(actual!=null) {
+			if(actual.getNum()==n) {
+				result = true;
+				break;
+			}else {
+				actual = actual.getNext();
+			}
+		}
+		return result;
+	}
+	
+	public boolean searchLinkedListR(long n) {
+		if(linkedList==null) {
+			return false;
+		}else {
+			LinkedList actual = linkedList;
+			return searchLinkedListR(n,actual);
+		}
+	}
+	
+	private boolean searchLinkedListR(long n, LinkedList actual) {
+		if(actual==null) {
+			return false;
+		}
+		if(actual.getNum()==n) {
+			return true;
+		}else {
+			actual = actual.getNext();
+			return searchLinkedListR(n,actual);
+		}
+	}
+	
+	public boolean searchBinaryTree(long n) {
+		Boolean result = false;
+		if(binaryTree==null) {
+			return false;
+		}else {
+			SearchBinaryTree actual = binaryTree;
+			while(actual!=null) {
+				if(n<actual.getNum()) {
+					actual = actual.getLeft();
+				}else if(n>actual.getNum()){
+					actual = actual.getRight();
+				}else {
+					result = true;
+				}
+			}
+		}
+		return result;
+	}
+	
+	public boolean searchBinaryTreeR(long n) {
+		return searchBinaryTreeR(n,binaryTree);
+	}
+	
+	private boolean searchBinaryTreeR(long n,SearchBinaryTree actual) {
+		if(actual==null) {
+			return false;
+		}
+		if(n<actual.getNum()) {
+			actual = actual.getLeft();
+		
+		}else if(n>actual.getNum()){
+			actual = actual.getRight();
+		}else {
+			return true;
+		}
+		return searchBinaryTreeR(n,actual);
+	}
+	
+	public boolean delateArrayList(long n) {
+		boolean result = false;
+		for (int i = 0; i < arrayList.size(); i++) {
+			if(arrayList.get(i)==n) {
+				arrayList.remove(i);
+				result = true;
+				break;
+			}
+		}
+		return result;
+	}
+	
+	public boolean delateArrayListR(long n) {
+		if(arrayList.size()>0) {
+			return delateArrayListR(n,arrayList.size()-1);
+		}else {
+			return false;
+		}
+	}
+	
+	private boolean delateArrayListR(long n,int i) {
+		if(arrayList.get(i)==n) {
+			return true;
+		}
+		if(i>0) {
+			return delateArrayListR(n,i-1);
+		}else {
+			return false;
+		}
+		
+		
+	}
+	
+	public boolean delateLinkedList(long n) {
+		LinkedList actual = linkedList;
+		boolean result = false;
+		if(actual.getNum()==n) {
+			linkedList = linkedList.getNext();
+		}else {
+			LinkedList prev = actual;
+			actual = actual.getNext();
+			while(actual!=null && result ==false) {
+				if(actual.getNum()==n) {
+					prev.setNext(actual.getNext());
+					result = true;
+				}else {
+					prev = actual;
+					actual = actual.getNext();
+				}
+			}
+		}
+		return result;
+		
+	}
+	
+	public boolean delateLinkedListR(long n) {
+		if(linkedList==null) {
+			return false;
+		}else {
+			if(linkedList.getNum()==n) {
+				linkedList = linkedList.getNext();
+				return true;
+			}else {
+				LinkedList actual = linkedList.getNext();
+				LinkedList prev = linkedList;
+				return delateLinkedListR(n,actual,prev);
+			}	
+		}
+	}
+	
+	private boolean delateLinkedListR(long n, LinkedList actual,LinkedList prev) {
+		if(actual == null) {
+			return false;
+		}
+		if(actual.getNum()==n) {
+			prev.setNext(actual.getNext());
+			return true;
+		}else {
+			prev = actual;
+			actual = actual.getNext();
+			return delateLinkedListR(n,actual,prev);
+		}
+	}
+	
+	public boolean delateBinaryTree(long n) {
+		SearchBinaryTree actual = binaryTree;
+		if(actual.getNum()==n){
+			SearchBinaryTree nuevo = actual.getRight();
+			
+		}
+		boolean result = false;
+		while(actual!=null && result ==false) {
+			if(actual.getNum()==n){
+				SearchBinaryTree nuevo = actual.getRight();
 				
 			}
 		}
 	}
+	
 	public ArrayList<Long> getArrayList() {
 		return arrayList;
 	}
